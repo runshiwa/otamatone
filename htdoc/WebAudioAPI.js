08e66a1e3482691563dccad847cde0c306d95574
runshiwa <nobuyuki@a.email.ne.jp>
2014-12-01 04:14:24 +0900

    publish master:a256994566b4f394128fd25d172dc5d925732e86

--- htdoc/WebAudioAPI.js
+++ htdoc/WebAudioAPI.js
@@ -2,7 +2,14 @@ var ac, g, o;
 var digital;
 var canvas;
 
-function start(){
+var octaveOffset = 1;
+var octaveRange = 1;
+var keyColor = [1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0];
+
+var controlByMouse = false;
+var controlByTouch = true;
+
+function start(name){
     ac = new AudioContext();
     g = ac.createGain();
     g.connect(ac.destination);
@@ -14,23 +21,64 @@ function start(){
     setFrequency(440);
     o.start();
 
-    canvas = document.getElementById("instrument");
-    //canvas.addEventListener("touchmove", draw);
-    //canvas.addEventListener("mousemove", draw);
-    canvas.addEventListener("mousedown", noteOn);
-    canvas.addEventListener("mouseup", noteOff);
-    canvas.addEventListener("touchstart", noteOn);
-    canvas.addEventListener("touchend", noteOff);
-    canvas.addEventListener("touchmove", modulate);
-    canvas.addEventListener("mousemove", modulate);
-
+    canvas = document.getElementById(name);
+    canvas.addEventListener("touchmove", function(event){
+	event.preventDefault();
+    }); 
+    if(controlByMouse){
+	//canvas.addEventListener("mousemove", draw);
+	canvas.addEventListener("mousedown", noteOn);
+	canvas.addEventListener("mouseup", noteOff);
+	canvas.addEventListener("mousemove", modulate);
+    }
+    if(controlByTouch){
+	//canvas.addEventListener("touchmove", draw);
+	canvas.addEventListener("touchstart", noteOn);
+	canvas.addEventListener("touchend", noteOff);
+	canvas.addEventListener("touchmove", modulate);
+    }
     canvas.addEventListener("resize", fitCanvas());
     fitCanvas();
 }
 
+function drawFret(canvas){
+    var context = canvas.getContext("2d");
+
+    var lastKey = octaveOffset * 12 + Math.round(octaveRange * -1 * 12);
+    var key = lastKey;
+    key %=  keyColor.length;
+    if(key < 0)
+	key += keyColor.length;
+    if(keyColor[key])
+	context.fillStyle = "white";
+    else
+	context.fillStyle = "black";
+    context.fillRect(0, 0, canvas.width - 0, canvas.height);
+    for(var x = 0; x < canvas.width; x++){
+	var w = (x - canvas.width / 2) / (canvas.width / 2);
+	key = octaveOffset * 12 + Math.round(octaveRange * w * 12);
+	if(lastKey != key){
+	    lastKey = key;
+	    key %= keyColor.length;
+	    if(key < 0)
+		key += keyColor.length;
+	    if(keyColor[key])
+		context.fillStyle = "white";
+	    else
+		context.fillStyle = "black";
+	    context.fillRect(x, 0, canvas.width - x, canvas.height);
+	    context.moveTo(x, 0);
+	    context.lineTo(x, canvas.height);
+	}
+    }
+    context.fill();
+    context.stroke();
+}
+
 function fitCanvas(){
-    canvas.width = innerWidth * 0.9;
-    canvas.height = innerHeight * 0.9;
+    canvas.width = innerWidth;
+    canvas.height = innerHeight * 0.8;
+    drawFret(canvas);
 }
 
 function setPower(value){
@@ -64,18 +112,23 @@ function setDetune(value){
 }
 
 function draw(event){
-    console.log(event);
+    //event.preventDefault();
 
-    var context = canvas.getContext("2d");
+    //console.log(event);
 
-    context.fillRect(event.offsetX, event.offsetY, 5, 5);
-    for(var i in event.touches){
-	context.fillRect(i.offsetX, i.offsetY, 5, 5);
+    var context = event.target.getContext("2d");
+
+    if(event.offsetX && event.offsetY)
+	context.fillRect(event.offsetX, event.offsetY, 5, 5);
+    for(var i = 0; i < event.targetTouches.length; i++){
+	document.control.message.value = "touch[" + i + "]=(" + event.targetTouches[i].pageX + ", " + event.targetTouches[i].pageY + ")";
+	context.fillRect(event.targetTouches[i].pageX - getOffsetLeft(event.target), event.targetTouches[i].pageY - getOffsetTop(event.target), 5, 5);
     }
 }
 
 function noteOn(event){
     setPower(1);
+    modulate(event);
 }
 
 function noteOff(event){
@@ -83,20 +136,52 @@ function noteOff(event){
 }
 
 function modulate(event){
-    var w = (event.offsetX - canvas.width / 2) / (canvas.width / 2);
-    var h = (event.offsetY - canvas.height / 2) / (canvas.height / 2);
+    var x, y;
+    if(event.offsetX && event.offsetY){
+	x = event.offsetX;
+	y = event.offsetY;
+    }
+    for(var i = 0; i < event.targetTouches.length; i++){
+	x = event.targetTouches[i].pageX - getOffsetLeft(event.target);
+	y = event.targetTouches[i].pageY - getOffsetTop(event.target);
+    }
+
+    var w = (x - canvas.width / 2) / (canvas.width / 2);
+    var h = (y - canvas.height / 2) / (canvas.height / 2);
     //console.log("w=" + w + " h=" + h);
 
     var f, d;
     if(!digital){
-	f = 440 * Math.pow(2, 1 + 3 * w);
+	f = 440 * Math.pow(2, octaveOffset + octaveRange * w);
     }
     else {
-	f = 440 * Math.pow(2, 1 + Math.round(3 * w * 12) / 12);
+	var key = octaveOffset * 12 + Math.round(octaveRange * w * 12);
+	document.control.message.value = key % keyColor.length;
+	f = 440 * Math.pow(2, key / 12);
     }
     d = 2 * 100 * -h;
     //d = 2 * 100 * Math.round(-h * 3) / 3;
 
     setFrequency(f);
     setDetune(d);
+}
+
+function getOffsetLeft(element){
+    var result = 0;
+    do {
+	if (!isNaN(element.offsetLeft)){
+            result += element.offsetLeft;
+	}
+    } while(element = element.offsetParent);
+    return result;
+}
+
+function getOffsetTop(element){
+    var result = 0;
+    do {
+	if (!isNaN(element.offsetTop)){
+            result += element.offsetTop;
+	}
+    } while(element = element.offsetParent);
+    return result;
 }
